bee8005668f0ca8351bf5c01f6ecc42253683008
1Mill <erik.ekberg@outlook.com>
2020-12-05 14:26:40 -0800

    Mess with JSDOM

--- services/crawl-url-for-content/index.js
+++ services/crawl-url-for-content/index.js
@@ -1,8 +1,10 @@
+const { JSDOM } = require('jsdom')
 const { v4: { createEventStream } } = require('@1mill/cloudevents')
 
 const lambda = createEventStream({ protocol: 'lambda' })
 const perform = async () => {
-	console.log('Hello world!')
+	const dom = new JSDOM(`<!DOCTYPE html><p>This is the content of the page!</p>`)
+	const test = dom.window.document.querySelector('p').textContent
+	console.log(test)
 }
 exports.handler = lambda.handler(perform)
-
