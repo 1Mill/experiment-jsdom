743722e8eaaaf3525f3782e7727d559474a984b3
1Mill <erik.ekberg@outlook.com>
2020-12-05 14:56:32 -0800

    Getting familiar with API

--- services/crawl-url-for-content/index.js
+++ services/crawl-url-for-content/index.js
@@ -3,8 +3,16 @@ const { v4: { createEventStream } } = require('@1mill/cloudevents')
 
 const lambda = createEventStream({ protocol: 'lambda' })
 const perform = async () => {
-	const dom = new JSDOM(`<!DOCTYPE html><p>This is the content of the page!</p>`)
-	const test = dom.window.document.querySelector('p').textContent
+	const dom = await JSDOM.fromURL('https://example.com', {
+		resources: 'usable',
+		runScripts: 'dangerously',
+	})
+
+	const test = dom.window.document.querySelector('h1').textContent
 	console.log(test)
+
+	const paragraphs = Array.from(dom.window.document.querySelectorAll('p')).map(htmlNode => htmlNode.textContent)
+	console.log(paragraphs)
+	console.log(paragraphs.length)
 }
 exports.handler = lambda.handler(perform)
