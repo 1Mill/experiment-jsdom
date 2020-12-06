8c16a77eec7ab431ea5ada9f71eb9894e922d203
1Mill <erik.ekberg@outlook.com>
2020-12-06 13:32:37 -0800

    Formatting + create editor page

--- services/editor/src/router/index.js
+++ services/editor/src/router/index.js
@@ -1,25 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+	{
+		path: '/',
+		name: 'Home',
+		component: Home
+	},
+	{
+		path: '/about',
+		name: 'About',
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+	},
+	{
+		path: '/editor',
+		name: 'Editor',
+		component: () => import(/* webpackChunkName: "editor" */ '../views/Editor.vue')
+	}
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+	history: createWebHistory(process.env.BASE_URL),
+	routes,
 })
 
 export default router
